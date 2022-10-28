12825e76f706c7aafd9889218e5ab61942a17afa
Nixon Ilya <95374645+MrN1x0n@users.noreply.github.com>
2022-10-28 12:56:12 +0300

    Update restake.sh

--- restake.sh
+++ restake.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
-#Special thanks 4 n0ok!
-
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
+DELAY=3600 # 1 hour
+DENOM=1000000
 
 for (( ;; )); do
-        BAL=$($NODE_NAME q  bank balances ${DELEGATOR});
-        echo -e "BALANCE: ${GREEN}${BAL}${NC} $TIKER\n"
-        echo -e "Claim rewards\n"
-        echo -e "${PASWD}\n${PASWD}\n" | $NODE_NAME tx distribution withdraw-rewards ${VALIDATOR} --chain-id $CHAIN_ID --from ${ACC_NAME} --gas 200000 --fees 400$TIKER -y 
-        for (( timer=10; timer>0; timer-- ))
-        do
-                printf "* sleep for ${RED}%02d${NC} sec\r" $timer
-                sleep 1
-        done
-        BAL=$($NODE_NAME query bank balances ${DELEGATOR} --node ${NODE} -o json | jq -r '.balances  | .[].amount');
-        BAL=$((BAL-1000000));
-        echo -e "BALANCE: ${GREEN}${BAL}${NC} $TIKER\n"
-        echo -e "Stake ALL\n"
-        echo -e "${PASWD}\n${PASWD}\n" | $NODE_NAME tx staking delegate ${VALIDATOR} ${BAL}$TIKER  --chain-id $CHAIN_ID --from ${ACC_NAME} --gas 200000 --fees 400$TIKER -y 
+        BAL=$(${NODE_NAME} query bank balances ${ADDRESS} -o json | jq -r --arg TIKER $TIKER '.balances[] | select(.denom==$TIKER) | .amount');
+        COMMISSION_U=$(${NODE_NAME} query distribution commission ${VALOPER} --output json | jq '.commission[] | select(.denom == "'"$TIKER"'") .amount' | bc)
+        REWARDS_U=$(${NODE_NAME} query distribution rewards ${ADDRESS} --output json | jq '.total[] | select(.denom == "'"$TIKER"'") .amount' | bc)
+        REWARDS_TOTAL_U=$(echo "(${COMMISSION_U}+${REWARDS_U}+${BAL})/1" | bc)
+
+        if (( ${REWARDS_TOTAL_U} > ${DENOM} )); then
+                ${NODE_NAME} tx distribution withdraw-rewards ${VALOPER} --from ${WALLET} --commission --fees 200${TIKER} -y
+                for (( timer=10; timer>0; timer-- ))
+                do
+                        printf "* sleep for ${RED}%02d${NC} sec\r" $timer
+                        sleep 1
+                done
+
+                # Leave 1 TIKER on our balance for commission
+                BAL=$((BAL-DENOM));
+                echo -e "BALANCE: ${GREEN}${BAL}${NC} ${TIKER}\n"
+                echo -e "Stake ALL\n"
+                if (( ${BAL} > $DENOM )); then
+                        ${NODE_NAME} tx staking delegate ${VALOPER} ${BAL}${TIKER} --from ${WALLET} --fees 200${TIKER} -y 
+                else
+                        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${NC} ${TIKER} ${BAL} < $DENOM\n"
+                fi
+        else
+                echo -e "BALANCE: ${GREEN_COLOR}${BAL}${NC}${TIKER} | ${BAL} < ${DENOM}"
+                echo -e "The balance is too small for staking\n"
+        fi
+
         for (( timer=${DELAY}; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED}%02d${NC} sec\r" $timer
                 sleep 1
         done
 done
-
